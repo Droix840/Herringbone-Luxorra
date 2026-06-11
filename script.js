7dfdb716029fd965200d0c41f8350ecb2f85255f
Droix840 <116375935+Droix840@users.noreply.github.com>
2026-06-11 21:29:23 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -7,10 +7,22 @@ const buttons = [
     document.getElementById("button_2"),
     document.getElementById("button_3")
 ];
-
+const contents = [
+    document.getElementById("content_0"),
+    document.getElementById("content_1"),
+    document.getElementById("content_2"),
+    document.getElementById("content_3")
+];
 menu.addEventListener("click", function() {
-    scroll.style.visibility = "visible";
+    scroll.classList.toggle("lathato");
 });
 lines.addEventListener("click", function() {
-    scroll.style.visibility = "visible";
+    scroll.classList.toggle("lathato");
 });
+function buttonclick(button, content){
+    content.classList.toggle("activeCont");
+}
+buttons[0].addEventListener("click",buttonclick(buttons[0],contents[0]))
+buttons[1].addEventListener("click",buttonclick(buttons[1],contents[1]))
+buttons[2].addEventListener("click",buttonclick(buttons[2],contents[2]))
+buttons[3].addEventListener("click",buttonclick(buttons[3],contents[3]))
