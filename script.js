f77bc066b7dd145aa3cfe1a0f72cb20cecef6c33
Droix840 <116375935+Droix840@users.noreply.github.com>
2026-06-11 11:36:57 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-var menu = document.getElementById("menu")
-var lines = document.getElementById("linesInMenu")
+const menu = document.getElementById("menu");
+const lines = document.getElementById("linesInMenu");
+const scroll = document.getElementById("scroll");
+const buttons = [
+    document.getElementById("button_0"),
+    document.getElementById("button_1"),
+    document.getElementById("button_2"),
+    document.getElementById("button_3")
+];
 
+menu.addEventListener("click", function() {
+    scroll.style.visibility = "visible";
+});
+lines.addEventListener("click", function() {
+    scroll.style.visibility = "visible";
+});
